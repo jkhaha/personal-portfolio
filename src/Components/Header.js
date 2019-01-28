657c3fc7d8c2915b1687019730fa77c5460bd16b
jkhaha <jha2@wellesley.edu>
2019-01-28 11:00:47 -0500

    fixed navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,11 @@ class Header extends Component {
   render(){
     return(
       <div className="header-component">
+        <div className="header-words">
         <a className='header-link' href='#about_me'> About Me </a> |
         <a className='header-link' href='#my_projects'> Projects </a> |
         <a className='header-link' href='#contact_me'> Contact Me </a>
+        </div>
       </div>
     )
   }
