578855483fa47ea5306865db85422127ab45a8db
jkhaha <jha2@wellesley.edu>
2019-01-27 23:19:11 -0500

    styling done

--- src/Components/ContactLinks.js
+++ src/Components/ContactLinks.js
@@ -8,9 +8,9 @@ class ContactLinks extends Component{
     return(
       <div>
       <span>
-        <a className="icon" target="_blank" rel="noopener noreferrer" href="https://github.com/jkhaha"><img src={Github} height='60px'></img></a>
-        <a className="icon" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jackie-ha/"><img src={LinkedIn} height='61.8px'></img></a>
-        <a className="icon" target="_blank" rel="noopener noreferrer" href="https://www.medium.com/@jkha0421"><img src={Medium} height='60px'></img></a>
+        <a className="icon" target="_blank" rel="noopener noreferrer" href="https://github.com/jkhaha"><img src={Github} height='50px'></img></a>
+        <a className="icon" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jackie-ha/"><img src={LinkedIn} height='50.5px'></img></a>
+        <a className="icon" target="_blank" rel="noopener noreferrer" href="https://www.medium.com/@jkha0421"><img src={Medium} height='50px'></img></a>
       </span>
     </div>
     )
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,9 @@ class Header extends Component {
   render(){
     return(
       <div className="header-component">
-        <a className='link' href='#about_me'> About Me </a> |
-        <a className='link' href='#my_projects'> Projects </a> |
-        <a className='link' href='#contact_me'> Contact Me </a>
+        <a className='header-link' href='#about_me'> About Me </a> |
+        <a className='header-link' href='#my_projects'> Projects </a> |
+        <a className='header-link' href='#contact_me'> Contact Me </a>
       </div>
     )
   }
